f1f49688a606f1431e8b75fbacfc1e3a49199eae
Adjective-Object <mhuan13@gmail.com>
2015-06-09 13:37:38 -0700

    args processing

--- src/mizu.hs
+++ src/mizu.hs
@@ -1,24 +1,45 @@
 module Main where
 
-import Data.List (foldl', intercalate, repeat, intersect)
-
--- Simple stuff for IO
 import System.IO (stdin)
 import System.Exit (exitWith, exitSuccess, ExitCode(..))
-
 import System.FilePath.Canonical
+import System.Environment (getProgName, getArgs)
 import System.Console.GetOpt (
     getOpt',
     OptDescr(..),
     ArgDescr(..),
     ArgOrder(Permute))
 
-import Control.Monad (when)
+import Data.List (foldl', intercalate, repeat, intersect)
+import Data.Map (Map, fromList)
+import Control.Monad (when, sequence)
+import Debug.Trace (trace)
+
+import Data.List.Split(splitOn)
+import Data.Aeson (decode)
+import Data.ByteString.Char8(pack)
+import Data.ByteString.Lazy(fromChunks)
+
+-- cast a value to string 
+traceVal x = trace (show x) x
+
+-- split path on image decoding error
+either' switch fail succeed = either fail succeed switch
+
+-- shorthand for exiting with an error msg
+exitWithError err = do
+    putStrLn err
+    exitWith (ExitFailure 1)
+
+
 
 data MaybeFlag = Error String | Flag FLAG
 
 -- options
 data FLAG = HELP
+    | DESTINATION_FIXED
+    | OUTPUT_DIRECTORY String
+    | COLOUR_MAPPING (Either String (Map String String))
     deriving (Show, Eq)
 
 (+\) a b = a ++ "\n" ++ b
@@ -27,60 +48,91 @@ data FLAG = HELP
 -- help text
 helpText :: String -> String
 helpText progName =
+    "A tool for generating xgcm templates from text files by" ++
+    "matching hex colours (#XXXXXX or #XXX) to"+\
+    "a provided colour set" +\
     "usage: " ++ progName ++ " [options] [files...]" +\
     "[options] one of" +\\
-        intercalate "\n    " optionStrings +\
-    "And files is one of the "
+        intercalate "\n\n    " optionStrings
 
 optionStrings :: [String]
 optionStrings =
-    let pairs =  map (\ (Option shorts longs _ desc) ->
-                    let fnames = map (\c -> ["-",c]) shorts
-                              ++ map (\l -> "--" ++ l) longs
-                    in (intercalate ", " fnames, desc)) options
-        maxflagstrlen = maximum $ map (\ (flagstr, _) -> length flagstr) pairs
-    in map (\ (flagstr, desc) ->
-        flagstr
-        ++ replicate (8 + maxflagstrlen - length flagstr) " "
-        ++ desc) pairs
+    let flagDescPairs =
+            map 
+                (\ (Option shorts longs _ desc) ->
+                    let flagNames = map (\c -> ['-',c]) shorts ++ 
+                                    map (\l -> "--" ++ l) longs
+                    in (intercalate ", " flagNames, 
+                        splitOn "\n" desc)) 
+                options
+        maxFlagStringLength = 
+            maximum $ map 
+                (\ (flagStr, _) -> length flagStr)
+                flagDescPairs
+        leftColumnPadding = "\n" ++ replicate (12 + maxFlagStringLength) ' '
+    in map
+        (\ (flagStr, descLines) ->
+            flagStr
+            ++ replicate (8 + maxFlagStringLength - length flagStr) ' '
+            ++ intercalate leftColumnPadding descLines)
+        flagDescPairs
+
 
 
 -- opt definitions
 helpOpt =
-    Option ["h"] ["help"]
+    Option "h" ["help"]
         (NoArg (Flag HELP))
         "display helptext and exit"
 
-outdirOpt =
-    Option ["o"] ["output-directory"]
-        (ReqArg mapping "PATH")
-        "Output directory (place the templates will be placed)."+\
-        "If left unspecified, templates will be left next to source files"
-    where mapping = id
-
 destdirOpt =
-    Option ["d"] ["template-destination"]
-        (ReqArg mapping "PATH")
-        "generation method (kmeans/..)"
-    where mapping = id
+    Option "d" ["template-destination"]
+        (NoArg (Flag DESTINATION_FIXED))
+        ("When specified, the generated templates will point to the"+\
+        "location of the original files on in the filesystem"+\
+        "by absolute path.")
 
-coloursOpt =
-    Option ["c"] ["colour", "color"]
-        (ReqArg mapping "FORMAT")
-        "output format (xresources/xgcm)"
-    where mapping arg = 
-            case arg of
-                "xresources" -> Flag FORMAT_XRESOURCES
-                "xgcm"       -> Flag FORMAT_XGCM
-                u            -> Error ("Unknown format " ++ show u)
+outdirOpt =
+    Option "o" ["output-directory"]
+        (ReqArg mapping "PATH")
+        ("Output directory (place the templates will be placed)."+\
+        "If left unspecified, templates will be left next to" +\
+        "source files")
+    where mapping str = Flag (OUTPUT_DIRECTORY str)
+
+colourMapOpt =
+    Option "o" ["output-directory"]
+        (ReqArg mapping "COLOUR_MAPPING")
+        ("a valid JSON string of the mapping between colour names to"+\
+         "hex codes. Example:"+\\
+             "{ \"red\" :  \"#FF0000\""+\\
+             ", \"green\": \"#00FF00\""+\\
+             ", \"blue\":  \"#0000FF\" }"+\
+         "If it is not a valid JSON string, it will be treated as a"+\
+         "file path, and  the apprpriate file will be parsed as json")
+    where mapping str = Flag (COLOUR_MAPPING 
+                                (case (decode $ fromChunks $ pack str : []) of
+                                    Just a -> Right a
+                                    Nothing -> Left str))
+
+defaultColourMap = fromList 
+    [ ("black", "#000000")
+    , ("white", "#FFFFFF")
+    , ("green", "#00FF00")
+    , ("blue",  "#0000FF")
+    , ("red",   "#FF0000")
+    , ("yellow","#FFFF00")
+    , ("cyan",  "#00FFFF")
+    , ("purple","#FF00FF")
+    ]
 
 options :: [OptDescr MaybeFlag]
 options =
     [ helpOpt
-    , backgroundOpt
-    , methodOpt
-    , formatOpt ]
-
+    , outdirOpt
+    , destdirOpt
+    , colourMapOpt
+    ]
 
 getOpt :: [String] -> Either String ([FLAG], [String])
 getOpt argv =
@@ -90,25 +142,22 @@ getOpt argv =
                 Error _ -> True
                 _       -> False) flags
 
-    in if not null flagErrors -- if flagErrors is empty
+    in if not $ null flagErrors -- if flagErrors is empty
         then let flagErrorMessages = map (\ (Error s) -> s) flagErrors
             in Left ("Error in parsing flags:\n\t"
                         ++ intercalate "\n\t" flagErrorMessages)
-        else if not null errs
+        else if not $ null errs
             then Left ("Error parsing argv\n" ++ foldl' (++) "" errs)
             else let realFlags = map (\ (Flag f) -> f) flags
                 in Right (realFlags, nonopts)
 
--- split path on image decoding error
-either' switch fail succeed = either fail succeed switch
+
 
 printHelpText :: IO()
 printHelpText = do
     progName <- getProgName
     putStrLn $ helpText progName
     exitSuccess
-
-parseFiles :: []
 
 main :: IO()
 main = do
@@ -118,9 +167,13 @@ main = do
         (\opts -> do
             let (flags, filePaths) = opts
 
+            -- print helptext when no args or --help is one of the args
             when (HELP `elem` flags || null filePaths)
                 printHelpText
 
-            let canonicalPaths = map canonical filePaths
-            scanColours canonicalPaths
+            -- colourMap <- getColourmapFromOpts flags
+
+            canonicalPaths <- sequence $ map canonical filePaths
+
+            putStrLn $ show canonicalPaths
             )
